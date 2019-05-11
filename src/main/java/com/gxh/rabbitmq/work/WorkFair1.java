7876b503dd29b2bd0f63dd2686869f3d204b6ef1
gongxuhui <gongxuhui@aliyun.com>
2019-05-11 21:53:29 +0400

    work model 公平分发代码

---
+++ src/main/java/com/gxh/rabbitmq/work/WorkFair1.java
@@ -0,0 +1,46 @@
+package com.gxh.rabbitmq.work;
+
+import com.gxh.rabbitmq.utils.ConnectionUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * work model 公平分发 信息接收
+ */
+public class WorkFair1 {
+    private static final String QUEUE_NAME="test_work_queue";
+    public void workReceive() throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        channel.basicQos(1);//确保一次只分发一个
+        //定义一个消费者
+        DefaultConsumer defaultConsumer = new DefaultConsumer(channel){
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                //super.handleDelivery(consumerTag, envelope, properties, body);
+                String message = new String(body,"utf-8");
+                System.out.println("[1] receive message===>"+ message);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }finally {
+                    System.out.println("done===>"+envelope.getDeliveryTag());
+
+                    channel.basicAck(envelope.getDeliveryTag(),false);
+                }
+            }
+        };
+        channel.basicConsume(QUEUE_NAME,false,defaultConsumer);
+
+    }
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        WorkFair1 workReceive = new WorkFair1();
+        workReceive.workReceive();
+    }
+
+}
---
+++ src/main/java/com/gxh/rabbitmq/work/WorkFairSend.java
@@ -0,0 +1,44 @@
+package com.gxh.rabbitmq.work;
+
+import com.gxh.rabbitmq.utils.ConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * rabbitmq work模型 消息发送，其实也simple模型发送消息的方式一样，只是接收不同
+ * 能者多劳，公平分发（fair dispatch）
+ *
+ */
+public class WorkFairSend {
+    private static final String QUEUE_NAME="test_work_queue";
+    public void workFairSend() throws IOException, TimeoutException {
+        //获取连接
+        Connection connection = ConnectionUtil.getConnection();
+        //创建信道
+        Channel channel = connection.createChannel();
+        //声明队列
+        channel.queueDeclare(QUEUE_NAME,false,false,false,null);
+        /**
+         * 设置参数，确保每个消费者在确认消息之前，消息队列不发送下一个消息到消费者。一次只处理一个消息
+         *
+         * 限制发送给同一个消费者不得超过一条消息
+         */
+        int prefectchCount = 1;
+        channel.basicQos(prefectchCount);
+        for (int i=0; i< 50; i++){
+            String message = "我的未来很无敌"+i;
+            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            System.out.println(message);
+        }
+        channel.close();
+        connection.close();
+    }
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        WorkFairSend workRoundSend = new WorkFairSend();
+        workRoundSend.workFairSend();
+    }
+}
