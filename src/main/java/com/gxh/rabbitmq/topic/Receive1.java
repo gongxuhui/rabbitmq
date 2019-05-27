617d7a81bdfd8ab2ba4dfa3b4527379ffec07e67
gongxuhui <gongxuhui@aliyun.com>
2019-05-27 13:46:29 +0800

    '主题模式消息发送代码'

---
+++ src/main/java/com/gxh/rabbitmq/topic/Receive1.java
@@ -0,0 +1,11 @@
+package com.gxh.rabbitmq.topic;
+
+/**
+ * @program: rabbitmq
+ * @author: Mr.gong
+ * @create: 2019-05-27 12:45
+ * @description: ${description}
+ **/
+public class Receive1 {
+
+}
--- src/main/java/com/gxh/rabbitmq/topic/TopicSend.java
+++ src/main/java/com/gxh/rabbitmq/topic/TopicSend.java
@@ -1,5 +1,13 @@
 package com.gxh.rabbitmq.topic;
 
+import com.gxh.rabbitmq.utils.ConnectionUtil;
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 /**
  * 主题模式 消息发送  topic model
  *
@@ -10,4 +18,17 @@ package com.gxh.rabbitmq.topic;
  * #（井号） 0个或者多个单词
  **/
 public class TopicSend {
+    private static final String EXCHANGE_NAME = "test_exchange_topic";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        //申明一个交换机
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
+        String routingKey = "";
+        String message = "巩旭辉今天到此一游";
+        channel.basicPublish(EXCHANGE_NAME,routingKey,null,message.getBytes());
+        channel.close();
+        connection.close();
+    }
 }
